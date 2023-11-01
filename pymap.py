def1e62cb77a6e0105fe5483ab18a83ad1b81973
geographieactuelle <140368141+geographieactuelle@users.noreply.github.com>
2023-11-01 17:11:03 -0400

    Added from_geojson()

--- pymap.py
+++ pymap.py
@@ -159,26 +159,26 @@ class SVGMap(dict):
         file.close()
 
 
-def from_shapefile(file: str, record_id: int, res=5, isl=1, countries=None, zoom=1, translate=None, height=180,
+def from_shapefile(file: str, record_id: int, res=5, isl=1, poly_keep=None, zoom=1, translate=None, height=180,
                    width=360, extra_smooth=False) -> Coordinates:
 
-    if countries is None:
-        countries = []
+    if poly_keep is None:
+        poly_keep = []
 
     # Open shapefile with PySHP
 
     shp = shapefile.Reader(file)
     shapes = shp.shapes()
-    # Define some variables
+
     svg = []
     record_list = []
-    # Counter variable to track progress
+
     i = 0
     # Loop through each country or territory
     for country in shapes:
-        i = i + 1
+        i += 1
         # Restrict parsing to specific countries
-        if (shp.shapeRecord(i - 1).record[record_id] in countries) or countries == []:
+        if (shp.shapeRecord(i - 1).record[record_id] in poly_keep) or poly_keep == []:
             # Track progress
             print(i)
             # Reset/define some variables
@@ -213,9 +213,76 @@ def from_shapefile(file: str, record_id: int, res=5, isl=1, countries=None, zoom
                     # Round data as before and save the point
                     main.append(data)
                     rounded_data = [str(round(long * res) / res), str(round(lat * res) / res)]
-                    if not rounded_data in rounded:
-                        if (not extra_smooth) or len(rounded) == 0 or ((not rounded[-1][0] == rounded_data[0]) and (not rounded[-1][1] == rounded_data[1])):
+                    if rounded_data not in rounded:
+                        if (not extra_smooth) or len(rounded) == 0 or (
+                                not rounded[-1][0] == rounded_data[0] and not rounded[-1][1] == rounded_data[1]):
                             rounded.append(rounded_data)
         record_list.append(shp.shapeRecord(i - 1).record[record_id].replace(' ', ''))
     # Create object from the points
     return Coordinates(svg, record_list, zoom, translate, height, width)
+
+
+def _get_geojson_feature_list(geojson: dict, res: int, isl: int) -> list:
+    if geojson['type'] == 'FeatureCollection':
+        poly_list = []
+        for poly in geojson['features']:
+            poly_list += _get_geojson_feature_list(poly, res, isl)
+        return poly_list
+
+    elif geojson['type'] == 'Feature':
+
+        if geojson['geometry']['type'] == 'Polygon':
+            geojson['geometry']['coordinates'] = [geojson['geometry']['coordinates']]
+            geojson['geometry']['type'] = 'MultiPolygon'
+
+        if geojson['geometry']['type'] == 'MultiPolygon':
+            new_poly_list = []
+            for polygon in geojson['geometry']['coordinates'][0]:
+                polygon_fixed = []
+                polygon_str = []
+                for_deletion = []
+                for i in range(0, len(polygon)):
+                    coord = polygon[i]
+                    coord[0] += 180
+                    coord[1] += 90
+                    coord[0] = round(coord[0] * res) / res
+                    coord[1] = round(coord[1] * res) / res
+                    str_coord = str(coord[0]) + ',' + str(coord[1])
+                    if str_coord in polygon_str:
+                        for_deletion.insert(0, i)
+                    else:
+                        polygon_str.append(str_coord)
+                        polygon_fixed.append(coord)
+                for i in for_deletion:
+                    del polygon[i]
+                new_poly = {'type': 'MultiPolygon', 'coordinates': polygon_fixed, 'properties': geojson['properties']}
+                if len(new_poly['coordinates']) > isl:
+                    new_poly_list.append(new_poly)
+            return new_poly_list
+
+        else:
+            return []
+
+    else:
+        raise TypeError('Not a valid geojson file')
+
+
+def from_geojson(file: str, record_id: str, res=5, isl=1, poly_keep=None, zoom=1, translate=None,
+                 height=180, width=360) -> Coordinates:
+    if poly_keep is None:
+        poly_keep = []
+    with open(file) as geojson_txt:
+        geojson = json.load(geojson_txt)
+    feature_list = _get_geojson_feature_list(geojson, res, isl)
+    coordinates_list = []
+    record_list = []
+    for poly in feature_list:
+        record = poly['properties'][record_id]
+        if record in poly_keep or poly_keep == []:
+            coordinates_list.append(poly['coordinates'])
+            record_list.append(record.replace(' ', ''))
+    for i in range(0, len(coordinates_list)):
+        coordinates_list[i] = {'Points': coordinates_list[i],
+                               'Country': record_list[i]}
+
+    return Coordinates(coordinates_list, record_list, zoom, translate, height, width)
